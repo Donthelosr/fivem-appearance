d0a49f3ee465ce58842aaae2b578f7a0f5c31f3e
Don <36248274+Donthelosr@users.noreply.github.com>
2023-01-18 14:05:04 -0500

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,49 @@
 fx_version "cerulean"
-game { "gta5" }
+game "gta5"
 
 author 'snakewiz'
 description 'A flexible player customization script for FiveM.'
 repository 'https://github.com/pedr0fontoura/fivem-appearance'
-version '1.3.0'
+version 'main'
+lua54 'yes'
 
-client_script 'game/dist/index.js'
+client_scripts {
+  'game/dist/index.js',
+  'client/blips.lua',
+  'client/props.lua',
+  'client/client.lua',
+  'client/backward-events.lua',
+  '@PolyZone/client.lua',
+  '@PolyZone/BoxZone.lua',
+  '@PolyZone/ComboZone.lua',
+  'migrate/client/client.lua'
+}
+
+server_scripts {
+  'server/server.lua',
+  'server/permissions.lua',
+  '@oxmysql/lib/MySQL.lua',
+  'migrate/server/server.lua',
+}
+
+shared_scripts {
+  'shared/config.lua'
+}
 
 files {
   'web/dist/index.html',
   'web/dist/assets/*.js',
   'locales/*.json',
   'peds.json',
-  'tattoos.json'
+  'tattoos.json',
+  'blacklist.json',
+  'theme.json'
 }
 
 ui_page 'web/dist/index.html'
+
+provide 'qb-clothing'
+
+dependencies {
+  'qb-core',
+}
